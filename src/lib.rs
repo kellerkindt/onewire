82ae2af3209aab71f2a7687bec88f209e6c5e54e
Michael Watzko <michael@watzko.de>
2018-02-17 21:30:35 +0100

    Use embedded_hal::digital::OutputPint and embedded_hal::blocking::delay::DelayUs instead of the wanky inhouse solution

--- src/lib.rs
+++ src/lib.rs
@@ -4,33 +4,22 @@
 #![crate_name = "onewire"]
 
 
+extern crate embedded_hal as hal;
 
-
-pub trait OpenDrainOutput {
-
-    fn drain_low(&mut self);
-
-    fn float_high(&mut self);
-
-    fn is_low(&self) -> bool {
-        !self.is_high()
-    }
-
-    fn is_high(&self) -> bool;
-
-    fn delay_us(&mut self, us: u16);
-}
-
+use hal::digital::OutputPin;
+use hal::blocking::delay::DelayUs;
 
 #[derive(Debug)]
 pub enum OneWireError {
     WireNotHigh,
 }
 
+#[derive(Clone, PartialOrd, PartialEq)]
 pub struct OneWireDevice {
     pub address: [u8; 8]
 }
 
+#[derive(Clone)]
 pub struct OneWireDeviceSearch {
     address: [u8; 8],
     last_discrepancy: u8,
@@ -55,30 +44,30 @@ impl OneWireDeviceSearch {
     }
 }
 
-pub struct OneWire<O: OpenDrainOutput + Sized> {
-    output: O,
+pub struct OneWire<'a> {
+    output: &'a mut OutputPin,
     parasite_mode: bool,
 }
 
-impl<O: OpenDrainOutput + Sized> OneWire<O> {
+impl<'a> OneWire<'a> {
 
-    pub fn new(output: O, parasite_mode: bool) -> OneWire<O> {
+    pub fn new(output: &'a mut OutputPin, parasite_mode: bool) -> OneWire<'a> {
         OneWire {
             output,
             parasite_mode,
         }
     }
 
-    pub fn search_next(&mut self, search: &mut OneWireDeviceSearch) -> Result<Option<OneWireDevice>, OneWireError> {
-        self.search(search, 0xF0)
+    pub fn search_next(&mut self, search: &mut OneWireDeviceSearch, delay: &mut DelayUs<u16>) -> Result<Option<OneWireDevice>, OneWireError> {
+        self.search(search, delay, 0xF0)
     }
 
-    pub fn search_next_alarmed(&mut self, search: &mut OneWireDeviceSearch) -> Result<Option<OneWireDevice>, OneWireError> {
-        self.search(search, 0xEC)
+    pub fn search_next_alarmed(&mut self, search: &mut OneWireDeviceSearch, delay: &mut DelayUs<u16>) -> Result<Option<OneWireDevice>, OneWireError> {
+        self.search(search, delay, 0xEC)
     }
 
     /// Heavily inspired by https://github.com/ntruchsess/arduino-OneWire/blob/85d1aae63ea4919c64151e03f7e24c2efbc40198/OneWire.cpp#L362
-    fn search(&mut self, rom: &mut OneWireDeviceSearch, command: u8) -> Result<Option<OneWireDevice>, OneWireError> {
+    fn search(&mut self, rom: &mut OneWireDeviceSearch, delay: &mut DelayUs<u16>, command: u8) -> Result<Option<OneWireDevice>, OneWireError> {
         let mut id_bit_number = 1_u8;
         let mut last_zero = 0_u8;
         let mut rom_byte_number = 0_usize;
@@ -89,15 +78,15 @@ impl<O: OpenDrainOutput + Sized> OneWire<O> {
 
 
         if !rom.last_device_flag {
-            if !self.reset()? {
+            if !self.reset(delay)? {
                 return Ok(None);
             }
 
-            self.write_byte(command, false);
+            self.write_byte(delay, command, false);
 
             while rom_byte_number < 8 {
-                let id_bit = self.read_bit();
-                let cmp_id_bit = self.read_bit();
+                let id_bit = self.read_bit(delay);
+                let cmp_id_bit = self.read_bit(delay);
 
                 // no device?
                 if id_bit && cmp_id_bit {
@@ -130,7 +119,7 @@ impl<O: OpenDrainOutput + Sized> OneWire<O> {
                     rom.address[rom_byte_number] &= !rom_byte_mask;
                 }
 
-                self.write_bit(search_direction);
+                self.write_bit(delay, search_direction);
 
                 id_bit_number += 1;
                 rom_byte_mask <<= 1;
@@ -172,81 +161,83 @@ impl<O: OpenDrainOutput + Sized> OneWire<O> {
     /// Returns Err(WireNotHigh) if the wire seems to be shortened,
     /// Ok(true) if presence pulse has been received and Ok(false)
     /// if no other device was detected but the wire seems to be ok
-    pub fn reset(&mut self) -> Result<bool, OneWireError> {
+    pub fn reset(&mut self, delay: &mut DelayUs<u16>) -> Result<bool, OneWireError> {
         // let mut cli = DisableInterrupts::new();
         self.set_input();
         // drop(cli);
 
-        self.ensure_wire_high()?;
+        self.ensure_wire_high(delay)?;
         // cli = DisableInterrupts::new();
         self.write_low();
         self.set_output();
 
         // drop(cli);
-        self.delay_us(480);
+        delay.delay_us(480);
         // cli = DisableInterrupts::new();
         self.set_input();
 
         let mut val = false;
         for _ in 0..7 {
-            self.delay_us(10);
+            delay.delay_us(10);
             val |= !self.read();
         }
         // drop(cli);
-        self.delay_us(410);
+        delay.delay_us(410);
         Ok(val)
     }
 
-    fn ensure_wire_high(&mut self) -> Result<(), OneWireError> {
+    fn ensure_wire_high(&mut self, delay: &mut DelayUs<u16>) -> Result<(), OneWireError> {
         for _ in 0..125 {
             if self.read() {
                 return Ok(());
             }
-            self.delay_us(2);
+            delay.delay_us(2);
         }
         Err(OneWireError::WireNotHigh)
     }
 
-    pub fn read_bytes(&self, dst: &mut [u8]) {
+    pub fn read_bytes(&mut self, delay: &mut DelayUs<u16>, dst: &mut [u8]) {
         for i in 0..dst.len() {
-            dst[i] = self.read_byte();
+            dst[i] = self.read_byte(delay);
         }
     }
 
-    fn read_byte(&self) -> u8 {
+    fn read_byte(&mut self, delay: &mut DelayUs<u16>) -> u8 {
         let mut byte = 0_u8;
         for _ in 0..8 {
             byte <<= 1;
-            byte |= self.read_byte();
+            if self.read_bit(delay) {
+                byte |= 0x01;
+            }
         }
         byte
     }
 
-    fn read_bit(&mut self) -> bool {
+    fn read_bit(&mut self, delay: &mut DelayUs<u16>) -> bool {
         // let cli = DisableInterrupts::new();
         self.set_output();
         self.write_low();
-        self.delay_us(3);
+        delay.delay_us(3);
         self.set_input();
-        self.delay_us(10);
+        delay.delay_us(10);
         let val = self.read();
         // drop(cli);
-        self.delay_us(53);
+        delay.delay_us(53);
         val
     }
 
-    pub fn write(&mut self, bytes: &[u8]) {
+    pub fn write(&mut self, delay: &mut DelayUs<u16>, bytes: &[u8]) {
         for b in bytes {
-            self.write_byte(*b, false);
+            self.write_byte(delay, *b, false);
         }
         if !self.parasite_mode {
             self.disable_parasite_mode();
         }
     }
 
-    fn write_byte(&mut self, mut byte: u8, parasite_mode: bool) {
+    fn write_byte(&mut self, delay: &mut DelayUs<u16>, mut byte: u8, parasite_mode: bool) {
         for _ in 0..8 {
-            self.write_bit((byte & 0x01) == 0x01);
+            self.write_bit(delay, (byte & 0x01) == 0x01);
             byte >>= 1;
         }
         if !parasite_mode {
@@ -254,14 +245,14 @@ impl<O: OpenDrainOutput + Sized> OneWire<O> {
         }
     }
 
-    fn write_bit(&mut self, high: bool) {
+    fn write_bit(&mut self, delay: &mut DelayUs<u16>, high: bool) {
         // let cli = DisableInterrupts::new();
         self.write_low();
         self.set_output();
-        self.delay_us(if high {10} else {65});
+        delay.delay_us(if high {10} else {65});
         self.write_high();
         // drop(cli);
-        self.delay_us(if high {55} else {5})
+        delay.delay_us(if high {55} else {5})
     }
 
 
@@ -271,27 +262,28 @@ impl<O: OpenDrainOutput + Sized> OneWire<O> {
         self.write_low();
     }
 
+    #[inline]
     fn set_input(&mut self) {
-        self.output.float_high()
+        self.output.set_high()
     }
 
+    #[inline]
     fn set_output(&mut self) {
         // nothing to do?
     }
 
+    #[inline]
     fn write_low(&mut self) {
-        self.output.drain_low()
+        self.output.set_low()
     }
 
+    #[inline]
     fn write_high(&mut self) {
-        self.output.float_high()
+        self.output.set_high()
     }
 
+    #[inline]
     fn read(&self) -> bool {
         self.output.is_high()
-    }
-    
-    fn delay_us(&mut self, us: u16) {
-        self.output.delay_us(us)
     }
 }
